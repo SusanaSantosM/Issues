125e63fc4b9055381253bee86f701086b3b58ffb
Lucia <lposadagonzalez@danielcastelao.org>
2024-04-26 10:07:12 +0200

    close #3 , arreglao x2 :D

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hola Universo")
+        System.out.printf("Hola Universo");
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
